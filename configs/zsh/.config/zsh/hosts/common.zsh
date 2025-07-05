0a31f9e75c55c7e7751a913d25ef58d6f03a5b8c
sholt0r <sholtor@kobu.au>
2025-07-05 18:59:14 +1000

    Tiling WMs go brrr.

--- configs/zsh/.config/zsh/hosts/common.zsh
+++ configs/zsh/.config/zsh/hosts/common.zsh
@@ -11,6 +11,22 @@ export EDITOR=nvim
 
 bindkey -e
 
+# Standard keys
+bindkey "^[[3~" delete-char        # Del key
+bindkey "^H" backward-delete-char  # Backspace (usually already set)
+
+# Home/End fallback compatibility
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
+bindkey "^[OH" beginning-of-line
+bindkey "^[OF" end-of-line
+
+# Ctrl + Arrow keys
+bindkey "^[[1;5C" forward-word     # Ctrl + →
+bindkey "^[[1;5D" backward-word    # Ctrl + ←
+
 # TMUX Sessionizer
 bindkey -s ^f "tmux-sessionizer\n"
 bindkey -s '\eh' "tmux-sessionizer -s 0\n"
